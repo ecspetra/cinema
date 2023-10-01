afb92a5968098a67c0bf9a58186a99eacd3dca8f
ecspetra <ecspetra@gmail.com>
2023-10-01 10:50:23 -0700

    Working on parallax effect

--- src/components/TopBanner/index.tsx
+++ src/components/TopBanner/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import defaultMovieBg from '@/app/assets/images/default-movie-bg.jpg'
 import Image from '@/components/Images/Image'
 
@@ -7,13 +7,31 @@ type PropsType = {
 }
 
 const TopBanner: FC<PropsType> = ({ imageSrc }) => {
+	const [scrollY, setScrollY] = useState<number>(0)
+	const parallaxFactor = 0.5
+	const translateY = scrollY * parallaxFactor
+
+	useEffect(() => {
+		const handleScroll = () => {
+			setScrollY(window.scrollY)
+		}
+
+		window.addEventListener('scroll', handleScroll)
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll)
+		}
+	}, [])
+
 	return (
-		<div className='w-screen h-[600px] -mb-40 relative inset-x-1/2 -translate-x-1/2 after:w-full after:absolute after:bottom-0 after:h-full after:bg-gradient-to-t from-black overflow-hidden'>
-			<Image
-				className='aspect-[215/121] inset-x-1/2 inset-y-1/2 -translate-x-1/2 -translate-y-1/2'
-				src={`https://image.tmdb.org/t/p/original${imageSrc}`}
-				defaultImage={defaultMovieBg}
-			/>
+		<div className='w-screen h-[600px] -mb-40 relative inset-x-1/2 -translate-x-1/2 after:w-full after:absolute after:bottom-0 after:h-4/5 after:bg-gradient-to-t from-black overflow-hidden'>
+			<div style={{ transform: `translateY(${translateY}px)` }}>
+				<Image
+					className='aspect-[215/121] inset-x-1/2 top-1/4 -translate-x-1/2 -translate-y-1/4'
+					src={`https://image.tmdb.org/t/p/original${imageSrc}`}
+					defaultImage={defaultMovieBg}
+				/>
+			</div>
 		</div>
 	)
 }
