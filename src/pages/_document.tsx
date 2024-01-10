9e7821deeec65b31950012d14c6173fc6364a2c8
yuliiatkachenko <ecspetra@gmail.com>
2024-01-09 19:46:01 -0800

    refactor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ const Document = () => {
 				<link
 					rel='preconnect'
 					href='https://fonts.gstatic.com'
-					crossOrigin
+					crossOrigin=''
 				/>
 				<link
 					href='https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;600;700;800&display=swap'
