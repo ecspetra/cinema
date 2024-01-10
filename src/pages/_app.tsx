72e1b4c82e35885b3ebcaabd75efadbc635fa799
yuliiatkachenko <ecspetra@gmail.com>
2024-01-10 13:40:09 -0800

    build

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import App from 'next/app'
+import { FC } from 'react'
+import { AppProps } from 'next/app'
 import { AuthProvider } from '@/context/AuthProvider'
 import MainLayout from '../components/MainLayout/index'
 import { ModalProvider } from '@/context/ModalProvider'
 import Modal from '@/app/components/UI/Modal'
 import PageLoader from '@/app/components/PageLoader'
 
-class MyApp extends App {
-	render() {
-		const { Component, pageProps } = this.props
-		return (
-			<PageLoader>
-				<AuthProvider>
-					<ModalProvider>
-						<MainLayout>
-							<Component {...pageProps} />
-						</MainLayout>
-						<Modal />
-					</ModalProvider>
-				</AuthProvider>
-			</PageLoader>
-		)
-	}
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+	return (
+		<PageLoader>
+			<AuthProvider>
+				<ModalProvider>
+					<MainLayout>
+						<Component {...pageProps} />
+					</MainLayout>
+					<Modal />
+				</ModalProvider>
+			</AuthProvider>
+		</PageLoader>
+	)
 }
 
 export default MyApp
